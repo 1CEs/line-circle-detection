74a178f2efb662639778a226bceb6568e4a69eef
1CEs <isongwut.me@gmail.com>
2024-12-02 22:01:16 +0700

    chore: implement load & get_byte_image func

--- src/modules/image_module.py
+++ src/modules/image_module.py
@@ -1,10 +1,11 @@
 class ImageModule:
     
     def __init__(self) -> None:
-        self.byte_image: bytearray = None
-        self.image = None
+        self.__byte_image: bytearray = None
 
     def load(self, image_path: str) -> None:
         with open(image_path, 'rb') as file:
-            self.image = file
+            self.__byte_image = file.read()
 
+    def get_byte_image(self) -> bytearray:
+        return self.__byte_image
