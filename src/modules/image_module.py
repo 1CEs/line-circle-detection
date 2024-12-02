f13320720d9c21e6f2c3964b1a343653be3725cf
1CEs <isongwut.me@gmail.com>
2024-12-02 22:18:18 +0700

    chore: implemnt width & height image getter

--- src/modules/image_module.py
+++ src/modules/image_module.py
@@ -7,5 +7,14 @@ class ImageModule:
         with open(image_path, 'rb') as file:
             self.__byte_image = file.read()
 
+    def get_dimensions(self) -> tuple[int, int]:
+        byte: bytearray = self.get_byte_image()
+        if byte[:8] != b"\x89PNG\r\n\x1a\n":
+            raise ValueError("This image is not .PNG file.")
+        
+        width: int = int.from_bytes(byte[16:20], 'big')
+        height: int = int.from_bytes(byte[20:24], 'big')
+        return width, height
+
     def get_byte_image(self) -> bytearray:
         return self.__byte_image
