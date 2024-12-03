b85d4b736bcf8a15902eb3d8bcbdff9cd3de6c55
1CEs <isongwut.me@gmail.com>
2024-12-03 16:31:50 +0700

    chore: stop do fucking non-library thing (i give up)

--- src/main.py
+++ src/main.py
@@ -1,10 +1,8 @@
 from modules.image_module import ImageModule
 
 def main():
-    im = ImageModule()
-    im.load('./images/line.jpg')
-    w, h = im.get_dimensions()
-    print(f"{w} {h}")
+    pass
+    
 
 if __name__ == "__main__":
     main()
