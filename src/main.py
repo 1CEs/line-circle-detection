0d63cdf33e8f34d37623dda3162cd7d707688762
1CEs <isongwut.me@gmail.com>
2024-12-07 22:53:00 +0700

    chore: integrating modules

--- src/main.py
+++ src/main.py
@@ -4,25 +4,33 @@ from modules.hough_transform import HoughTransformModule
 from modules.display_module import DisplayModule
 
 def main() -> None:
-    display = DisplayModule()
-    for i in range(1, 4):
-        original = cv2.imread(f'./images/line/#{i}.jpg', cv2.IMREAD_COLOR)
-        grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    
+    for method_name in dir(EdgeDetectionModule):
 
-        edge_detector = EdgeDetectionModule(grayscale)
-        edges = edge_detector.canny()
+        if method_name.startswith("__"): continue
 
-        hough_transform = HoughTransformModule(edges, original.copy())
-        result_image, line_count = hough_transform.hough_line(255, 85)
+        method = getattr(EdgeDetectionModule, method_name)
 
-        display.load((
-            "Original Image", 
-            "Grayscale Image", 
-            "Edge Detected Image", 
-            f"Hough Line Image: {line_count} Lines"
-        ), (original, grayscale, edges, result_image), i)
+        if callable(method):
+            display = DisplayModule(f"{method_name.capitalize()} Method")
+            for i in range(1, 4):
+                original = cv2.imread(f'./images/line/#{i}.jpg', cv2.IMREAD_COLOR)
+                grayscale = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 
-    display.show()
+                edge_detector = EdgeDetectionModule(grayscale)
+                edges = method(edge_detector)
+
+                hough_transform = HoughTransformModule(edges, original.copy())
+                result_image, line_count = hough_transform.hough_line(255, 85)
+
+                display.load((
+                    "Original Image", 
+                    "Grayscale Image", 
+                    "Edge Detected Image", 
+                    f"Hough Line Image: {line_count} Lines"
+                ), (original, grayscale, edges, result_image), i)
+
+            display.show()
 
 if __name__ == "__main__":
     main()
